335730393f25c99c1e9f65996ba4bef49c8236c2
shandy4wd <shandy4wd@mail.ru>
2021-09-18 16:26:45 +0400

    add feedback section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
 const tabWidget = new TabWidget(document.getElementsByClassName('js-tabs')[0]);
+
+const sliderFeedback = new Swiper('.slider-feedback', {});
+const sliderFeedbackAuthor = new Swiper('.slider-feedback-author', {});
+const sliderFeedbackProduct = new Swiper('.slider-feedback-product', {});
+
+sliderFeedback.controller.control = [sliderFeedbackAuthor, sliderFeedbackProduct];
+sliderFeedbackAuthor.controller.control = [sliderFeedback, sliderFeedbackProduct];

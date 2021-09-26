24f87988a62f547ed6a7fb81576e4973cbf07fa0
shandy4wd <shandy4wd@mail.ru>
2021-09-26 17:37:25 +0400

    sync three sliders

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,25 @@
 const tabWidget = new TabWidget(document.getElementsByClassName('js-tabs')[0]);
 
 const sliderFeedback = new Swiper('.slider-feedback', {});
-const sliderFeedbackAuthor = new Swiper('.slider-feedback-author', {});
+const sliderFeedbackAuthor = new Swiper('.slider-feedback-author', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 const sliderFeedbackProduct = new Swiper('.slider-feedback-product', {});
 
-sliderFeedback.controller.control = [sliderFeedbackAuthor, sliderFeedbackProduct];
-sliderFeedbackAuthor.controller.control = [sliderFeedback, sliderFeedbackProduct];
+sliderFeedback.on('slideChange', function () {
+  sliderFeedbackProduct.slideTo(this.activeIndex);
+  sliderFeedbackAuthor.slideTo(this.activeIndex);
+});
+
+sliderFeedbackAuthor.on('slideChange', function () {
+  sliderFeedbackProduct.slideTo(this.activeIndex);
+  sliderFeedback.slideTo(this.activeIndex);
+});
+
+sliderFeedbackProduct.on('slideChange', function () {
+  sliderFeedback.slideTo(this.activeIndex);
+  sliderFeedbackAuthor.slideTo(this.activeIndex);
+});
